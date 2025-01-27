6cb52c6c9773973e4cc7be35e4f4f7fc7e0f42d2
Lucas Andrade <lucasantos.dev@gmail.com>
2025-01-26 22:54:53 -0300

    feat: finish ArtigoCompose

--- app/src/main/java/com/example/pdm_pratica_compose/MainActivity.kt
+++ app/src/main/java/com/example/pdm_pratica_compose/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.pdm_pratica_compose
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,6 +13,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,6 +40,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ArtigoSobreCompose(modifier: Modifier = Modifier) {
     Column {
+        Image(
+            painter = painterResource(id = R.drawable.bg_compose_background),
+            contentDescription = stringResource(id = R.string.app_name)
+        )
+
         Text(
             text = "Jetpack Compose tutorial",
             fontSize = 24.sp,
